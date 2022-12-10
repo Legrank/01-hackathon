63bc390cc9b2f3a28fc7cb35d18c9b1fb92a2715
Vova <vlasadovoj@gmail.com>
2022-12-10 12:45:23 +0300

    create clicks module

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,10 @@ export function random(min, max) {
   return Math.round(min - 0.5 + Math.random() * (max - min + 1))
 }
 
+export function formatTime(ms) {
+  return Number.parseFloat(ms / 1000).toFixed(2)
+}
+
 export function createMenuElement(text) {
   const menuElementHTML = document.createElement('li')
   menuElementHTML.className = 'menu-item'
